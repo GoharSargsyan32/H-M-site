16b333eb179b4717c7dc1980983e233ac65e1a84
Alex <alexkhachikyan2@gmail.com>
2023-06-09 13:46:42 +0400

    asd

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '@/styles/Home.module.scss'
 import Sale from '@/components/sale/sale'
 import Pool from '@/components/pool/pool'
